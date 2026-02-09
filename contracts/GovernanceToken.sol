a82099e18a4fae698abd835a1bd5b32a1e351493
Skuf <skuflow1@gmail.com>
2026-02-09 10:01:15 +0200

    Update GovernanceToken.sol

--- contracts/GovernanceToken.sol
+++ contracts/GovernanceToken.sol
@@ -1,23 +1,20 @@
-// base-governance/contracts/GovernanceToken.sol
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.20;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract GovernanceToken is ERC20 {
-    constructor() ERC20("Governance Token", "GOV") {
-        _mint(msg.sender, 10000000 * 10**18); // 10 million tokens
+contract GovToken is ERC20Votes, Ownable {
+    constructor() ERC20("BaseGovToken", "BGT") EIP712("BaseGovToken", "1") Ownable(msg.sender) {
+        _mint(msg.sender, 1_000_000e18);
     }
 
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
-    
-    function burn(address from, uint256 amount) external {
-        _burn(from, amount);
-    }
-    
-    function getGovernanceInfo() external view returns (uint256 totalSupply, uint256 ownerBalance) {
-        return (totalSupply(), balanceOf(owner()));
+
+    // required overrides
+    function _update(address from, address to, uint256 value) internal override(ERC20Votes) {
+        super._update(from, to, value);
     }
 }
