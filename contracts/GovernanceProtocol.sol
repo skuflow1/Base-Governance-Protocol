8d301884572b2b996d9fe6021e7095889c9cfe72
Skuf <skuflow1@gmail.com>
2026-02-10 16:35:16 +0200

    Update GovernanceProtocol.sol

--- contracts/GovernanceProtocol.sol
+++ contracts/GovernanceProtocol.sol
@@ -5,8 +5,7 @@ import "@openzeppelin/contracts/governance/Governor.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorCountingSimple.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotes.sol";
 import "@openzeppelin/contracts/governance/extensions/GovernorVotesQuorumFraction.sol";
-import "@openzeppelin/contracts/governance/extensions/GovernorTimelockControl.sol";
-import "@openzeppelin/contracts/governance/TimelockController.sol";
+
 
 contract BaseGovernor is
     Governor,
